2c4830359c05c1bfb1568f4a4745ac6653c76163
Domnick Miyeng <d.miyeng@gmail.com>
2020-12-04 11:41:36 -0600

    lab_02

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -14,6 +14,13 @@ arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0,arcade.color.BITTER_LIME)
 #Draw the Sun
 arcade.draw_circle_filled(500, 550, 40, arcade.color.ORANGE)
 
+#Draw car with wheels
+arcade.draw_rectangle_filled(600,150,140,70, arcade.color.BLACK)
+arcade.draw_circle_filled(500,110,50, arcade.color.BLUE)
+arcade.draw_circle_filled(500,110,45, arcade.color.LIGHT_BLUE)
+arcade.draw_circle_filled(650,90,30,arcade.color.BLUE)
+arcade.draw_circle_filled(650,90,25,arcade.color.LIGHT_BLUE)
+
 
 #  Finish and run
 arcade.finish_render()
